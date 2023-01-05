6d683e4e904f97fa2350a48a8685f3521fcf1d50
Aleksandr <alexandrmaslennikov85@gmail.com>
2023-01-06 00:15:12 +0700

    added go in Animal, fixed mate in Animal, added moveThe animal in Handler

--- src/main/java/ru/maslennikov/island/entities/animal/Animal.java
+++ src/main/java/ru/maslennikov/island/entities/animal/Animal.java
@@ -4,30 +4,21 @@ import ru.maslennikov.island.annotation.BasicCharacteristicsOrganism;
 import ru.maslennikov.island.entities.Organism;
 import ru.maslennikov.island.entities.animal.herbivorous.Herbivorous;
 import ru.maslennikov.island.entities.animal.predator.Predator;
-import ru.maslennikov.island.entities.plant.Plant;
 import ru.maslennikov.island.handler.Handler;
 import ru.maslennikov.island.island.Direction;
+import ru.maslennikov.island.island.Island;
 import ru.maslennikov.island.island.Location;
-import ru.maslennikov.island.setting.Setting;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class Animal implements Organism {
 
     private double saturationLevel;
     private double weight = ThreadLocalRandom.current().nextDouble((this.getMaxWeight() / 2), this.getMaxWeight());
-    private HashMap<Direction, Integer> directionOfMovement;
+    private Map<Direction, Integer> directionOfMovement;
 
     public String getName() {
         return this.getClass().getAnnotation(BasicCharacteristicsOrganism.class).name();
-    }
-
-    public void setDirectionOfMovement(HashMap<Direction, Integer> directionOfMovement) {
-        this.directionOfMovement = directionOfMovement;
     }
 
     public double getSaturationLevel() {
@@ -58,71 +49,23 @@ public abstract class Animal implements Organism {
         return this.getClass().getAnnotation(BasicCharacteristicsOrganism.class).maxTravelSpeed();
     }
 
-    private void chooseDirectionOfTravel(Location location) {
-        List<Animal> generalListAnimals = new ArrayList<>(location.getPredatorsOfLocation());
-        generalListAnimals.addAll(location.getHerbivorousOfLocation());
-        Direction[] directions = Direction.values();
-        int randomDirection = ThreadLocalRandom.current().nextInt(directions.length);
-        for (Animal animal : generalListAnimals) {
-            if (animal.getClass().getSimpleName().equalsIgnoreCase("caterpillar")) {
-                continue;
-            }
-            animal.directionOfMovement = new HashMap<>();
-            if (directions[randomDirection] == Direction.STAY_PUT) {
-                animal.directionOfMovement.put(directions[randomDirection], 0);
-            } else {
-                animal.directionOfMovement.put(directions[randomDirection], Direction.DOWN.getNumberOfSteps(animal));
-            }
-        }
+    public Map<Direction, Integer> getDirectionOfMovement() {
+        return directionOfMovement;
+    }
+
+    public void chooseDirectionOfTravel() {
+        Handler handler = new Handler();
+        directionOfMovement = handler.getDirectionOfTravel(this);
     }
 
     public void eat(Location location) {
         location.getLock().lock();
-        Handler handler = new Handler();
         try {
-            Iterator<Predator> iteratorPredators = handler.getOrganismIterator(location.getPredatorsOfLocation());
-            Iterator<Herbivorous> iteratorHerbivorouses = handler.getOrganismIterator(location.getHerbivorousOfLocation());
-            Iterator<Plant> iteratorPlants = handler.getOrganismIterator(location.getPlantsOfLocation());
-            int maxNumberOfPlantsEatenByOneAnimal = Setting.get().getMaxNumberOfPlantsEatenByOneAnimal();
+            Handler handler = new Handler();
             if (this instanceof Predator) {
-                while (iteratorPredators.hasNext()) {
-                    Predator nextPredator = iteratorPredators.next();
-                    if (handler.feedTheBody(this, nextPredator)) {
-                        iteratorPredators.remove();
-                        nextPredator = null;
-                    } else {
-                        continue;
-                    }
-                }
-                while (iteratorHerbivorouses.hasNext()) {
-                    Herbivorous nextHerbivorous = iteratorHerbivorouses.next();
-                    if (handler.feedTheBody(this, nextHerbivorous)) {
-                        iteratorHerbivorouses.remove();
-                        nextHerbivorous = null;
-                    } else {
-                        continue;
-                    }
-                }
+                handler.feedThePredator(location, this);
             } else if (this instanceof Herbivorous) {
-                while (iteratorHerbivorouses.hasNext()) {
-                    Herbivorous nextHerbivorous = iteratorHerbivorouses.next();
-                    if (handler.feedTheBody(this, nextHerbivorous)) {
-                        iteratorHerbivorouses.remove();
-                        nextHerbivorous = null;
-                    } else {
-                        continue;
-                    }
-                }
-                while (iteratorPlants.hasNext()) {
-                    Plant nextPlant = iteratorPlants.next();
-                    if (maxNumberOfPlantsEatenByOneAnimal > 0 && handler.feedTheBody(this, nextPlant)) {
-                        iteratorPlants.remove();
-                        maxNumberOfPlantsEatenByOneAnimal--;
-                        nextPlant = null;
-                    } else {
-                        continue;
-                    }
-                }
+                handler.feedTheHerbivorous(location, this);
             }
         } finally {
             location.getLock().unlock();
@@ -131,33 +74,21 @@ public abstract class Animal implements Organism {
 
     public void mate(Location location) {
         location.getLock().lock();
-        Handler handler = new Handler();
         try {
+            Handler handler = new Handler();
             if (this instanceof Predator) {
-                for (int i = 0; i < location.getPredatorsOfLocation().size(); i++) {
-                    if (handler.establishPossibilityOfHavingOffspring(location, this, location.getPredatorsOfLocation().get(i))) {
-                        handler.populateLocationWithPredators(location, this.getName(), Setting.get().getMaxNumberOfOffspring().get(this.getName()));
-                    } else {
-                        continue;
-                    }
-                }
-
+                handler.increaseNumberOfPredators(location, this);
             } else if (this instanceof Herbivorous) {
-                for (int i = 0; i < location.getHerbivorousOfLocation().size(); i++) {
-                    if (handler.establishPossibilityOfHavingOffspring(location, this, location.getHerbivorousOfLocation().get(i))) {
-                        handler.populateLocationWithHerbivorous(location, this.getName(), Setting.get().getMaxNumberOfOffspring().get(this.getName()));
-                    } else {
-                        continue;
-                    }
-                }
+                handler.increaseNumberOfHerbivorous(location, this);
             }
         } finally {
             location.getLock().unlock();
         }
     }
 
-    public HashMap<Direction, Integer> getDirectionOfMovement() {
-        return directionOfMovement;
+    public void go(Location location, Island island) {
+        Handler handler = new Handler();
+        handler.moveTheAnimal(island, location, this);
     }
 
     @Override
--- src/main/java/ru/maslennikov/island/handler/Handler.java
+++ src/main/java/ru/maslennikov/island/handler/Handler.java
@@ -2,17 +2,24 @@ package ru.maslennikov.island.handler;
 
 import ru.maslennikov.island.entities.Organism;
 import ru.maslennikov.island.entities.animal.Animal;
+import ru.maslennikov.island.entities.animal.herbivorous.Herbivorous;
+import ru.maslennikov.island.entities.animal.predator.Predator;
+import ru.maslennikov.island.entities.plant.Plant;
+import ru.maslennikov.island.island.Direction;
+import ru.maslennikov.island.island.Island;
 import ru.maslennikov.island.island.Location;
 import ru.maslennikov.island.maker.OrganismFactory.HerbivorousFactory;
 import ru.maslennikov.island.maker.OrganismFactory.PlantFactory;
 import ru.maslennikov.island.maker.OrganismFactory.PredatorFactory;
 import ru.maslennikov.island.setting.Setting;
-
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class Handler {
 
@@ -54,6 +61,54 @@ public class Handler {
         return false;
     }
 
+    public void feedThePredator(Location location, Animal eatingOrganism) {
+        Iterator<Predator> iteratorPredators = getOrganismIterator(location.getPredatorsOfLocation());
+        Iterator<Herbivorous> iteratorHerbivorouses = getOrganismIterator(location.getHerbivorousOfLocation());
+        while (iteratorPredators.hasNext()) {
+            Predator nextPredator = iteratorPredators.next();
+            if (feedTheBody(eatingOrganism, nextPredator)) {
+                iteratorPredators.remove();
+                nextPredator = null;
+            } else {
+                continue;
+            }
+        }
+        while (iteratorHerbivorouses.hasNext()) {
+            Herbivorous nextHerbivorous = iteratorHerbivorouses.next();
+            if (feedTheBody(eatingOrganism, nextHerbivorous)) {
+                iteratorHerbivorouses.remove();
+                nextHerbivorous = null;
+            } else {
+                continue;
+            }
+        }
+    }
+
+    public void feedTheHerbivorous(Location location, Animal eatingOrganism) {
+        Iterator<Herbivorous> iteratorHerbivorouses = getOrganismIterator(location.getHerbivorousOfLocation());
+        Iterator<Plant> iteratorPlants = getOrganismIterator(location.getPlantsOfLocation());
+        int maxNumberOfPlantsEatenByOneAnimal = Setting.get().getMaxNumberOfPlantsEatenByOneAnimal();
+        while (iteratorHerbivorouses.hasNext()) {
+            Herbivorous nextHerbivorous = iteratorHerbivorouses.next();
+            if (feedTheBody(eatingOrganism, nextHerbivorous)) {
+                iteratorHerbivorouses.remove();
+                nextHerbivorous = null;
+            } else {
+                continue;
+            }
+        }
+        while (iteratorPlants.hasNext()) {
+            Plant nextPlant = iteratorPlants.next();
+            if (maxNumberOfPlantsEatenByOneAnimal > 0 && feedTheBody(eatingOrganism, nextPlant)) {
+                iteratorPlants.remove();
+                maxNumberOfPlantsEatenByOneAnimal--;
+                nextPlant = null;
+            } else {
+                continue;
+            }
+        }
+    }
+
     public <T> Iterator<T> getOrganismIterator(List<T> organismOfLocation) {
         int minNumberOrganismOfLocation = 5;
         if (organismOfLocation.size() <= minNumberOrganismOfLocation) {
@@ -64,18 +119,20 @@ public class Handler {
         return organismOfLocation.subList(fromIndex, toIndex).iterator();
     }
 
-    public boolean establishPossibilityOfHavingOffspring(Location location, Animal firstAnimal, Animal secondAnimal) {
-        int newNumberOfAnimalsInLocation = location.getStatisticOfLocation().get(firstAnimal.getName()) + Setting.get().getMaxNumberOfOffspring().get(firstAnimal.getName());
-        if (Objects.nonNull(firstAnimal)
-                && Objects.nonNull(secondAnimal)
-                && firstAnimal.getName().equalsIgnoreCase(secondAnimal.getName())
-                && firstAnimal != secondAnimal
-                && Setting.get().getMaxNumberOfOrganismsOfThisTypeInLocation().get(firstAnimal.getName()) >= newNumberOfAnimalsInLocation) {
-            return true;
+    public Map<Direction, Integer> getDirectionOfTravel(Animal animal) {
+        Map<Direction, Integer> directionOfTravel = new HashMap<>();
+        Direction[] directions = Direction.values();
+        int randomDirection = ThreadLocalRandom.current().nextInt(directions.length);
+        if (animal.getClass().getSimpleName().equalsIgnoreCase("caterpillar")) {
+            return null;
         }
-        return false;
+        if (directions[randomDirection] == Direction.STAY_PUT) {
+            directionOfTravel.put(directions[randomDirection], 0);
+        } else {
+            directionOfTravel.put(directions[randomDirection], Direction.DOWN.getNumberOfSteps(animal));
+        }
+        return directionOfTravel;
     }
-
 
     public void populateLocationWithPredators(Location location, String name, int quantity) {
         if (name.equalsIgnoreCase("bear")
@@ -90,15 +147,6 @@ public class Handler {
             Collections.shuffle(location.getPredatorsOfLocation());
         } else {
             return;
-        }
-    }
-
-    public void plantLocationWithPlants(Location location, String name) {
-        if (name.equalsIgnoreCase("plant")) {
-            PlantFactory plantFactory = new PlantFactory();
-            for (int i = 0; i < Setting.get().getMaxNumberOfOrganismsOfThisTypeInLocation().get(name); i++) {
-                location.getPlantsOfLocation().add(plantFactory.createOrganism(name));
-            }
         }
     }
 
@@ -123,4 +171,81 @@ public class Handler {
         }
     }
 
+    public void plantLocationWithPlants(Location location, String name) {
+        if (name.equalsIgnoreCase("plant")) {
+            PlantFactory plantFactory = new PlantFactory();
+            for (int i = 0; i < Setting.get().getMaxNumberOfOrganismsOfThisTypeInLocation().get(name); i++) {
+                location.getPlantsOfLocation().add(plantFactory.createOrganism(name));
+            }
+        }
+    }
+
+    private boolean establishPossibilityOfHavingOffspring(Location location, Animal animal) {
+        int maxNumberOfOrganismsOfThisTypeInLocation = Setting.get().getMaxNumberOfOrganismsOfThisTypeInLocation().get(animal.getName());
+        int newNumberOfAnimalsInLocation = location.getStatisticOfLocation().get(animal.getName()) + Setting.get().getMaxNumberOfOffspring().get(animal.getName());
+        return getNumberOfThisAnimalInLocation(location, animal) > 1 && maxNumberOfOrganismsOfThisTypeInLocation >= newNumberOfAnimalsInLocation;
+    }
+
+    private int getNumberOfThisAnimalInLocation(Location location, Animal animal) {
+        if (animal instanceof Predator) {
+            return location.getPredatorsOfLocation().stream().filter(e -> e.getName().equalsIgnoreCase(animal.getName())).collect(Collectors.toList()).size();
+        }
+        return location.getHerbivorousOfLocation().stream().filter(e -> e.getName().equalsIgnoreCase(animal.getName())).collect(Collectors.toList()).size();
+    }
+
+    public void increaseNumberOfPredators(Location location, Animal animal) {
+        if (establishPossibilityOfHavingOffspring(location, animal)) {
+            PredatorFactory predatorFactory = new PredatorFactory();
+            for (int i = 0; i < Setting.get().getMaxNumberOfOffspring().get(animal.getName()); i++) {
+                location.getPredatorsOfLocation().add(predatorFactory.createOrganism(animal.getName()));
+            }
+        }
+    }
+
+    public void increaseNumberOfHerbivorous(Location location, Animal animal) {
+        if (establishPossibilityOfHavingOffspring(location, animal)) {
+            HerbivorousFactory herbivorousFactory = new HerbivorousFactory();
+            for (int i = 0; i < Setting.get().getMaxNumberOfOffspring().get(animal.getName()); i++) {
+                location.getHerbivorousOfLocation().add(herbivorousFactory.createOrganism(animal.getName()));
+            }
+        }
+    }
+
+    public void moveTheAnimal(Island island, Location location, Animal animal) throws ArrayIndexOutOfBoundsException {
+        Map<Direction, Integer> directionOfMovement = animal.getDirectionOfMovement();
+        int xCoordinate = location.getCoordinates().getxCoordinate();
+        int yCoordinate = location.getCoordinates().getyCoordinate();
+
+        if (directionOfMovement.containsKey(Direction.STAY_PUT)) {
+            return;
+        } else if (directionOfMovement.containsKey(Direction.UP)) {
+            yCoordinate -= directionOfMovement.get(Direction.UP);
+        } else if (directionOfMovement.containsKey(Direction.DOWN)) {
+            yCoordinate += directionOfMovement.get(Direction.DOWN);
+        } else if (directionOfMovement.containsKey(Direction.LEFT)) {
+            xCoordinate -= directionOfMovement.get(Direction.LEFT);
+        } else if (directionOfMovement.containsKey(Direction.RIGHT)) {
+            xCoordinate += directionOfMovement.get(Direction.RIGHT);
+        }
+        try {
+            island.getInhabitedIsland()[yCoordinate][xCoordinate].toString();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(animal.getName() + " he left the island where he was eaten by an animal of unknown type");
+            return;
+        }
+        try {
+            location.getLock().lock();
+            island.getInhabitedIsland()[yCoordinate][xCoordinate].getLock().lock();
+            if (animal instanceof Predator) {
+                island.getInhabitedIsland()[yCoordinate][xCoordinate].getPredatorsOfLocation().add((Predator) animal);
+                location.getPredatorsOfLocation().remove(animal);
+            } else if (animal instanceof Herbivorous) {
+                island.getInhabitedIsland()[yCoordinate][xCoordinate].getHerbivorousOfLocation().add((Herbivorous) animal);
+                location.getHerbivorousOfLocation().remove(animal);
+            }
+        } finally {
+            location.getLock().unlock();
+            island.getInhabitedIsland()[yCoordinate][xCoordinate].getLock().unlock();
+        }
+    }
 }
